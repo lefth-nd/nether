cf557eb5a0f084aee9a86a3690e4cb53131634ea
Jay <schu0527@flinders.edu.au>
2023-06-25 18:53:19 +0930

    img resize and button cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   });
 
   const btn =
-    "rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20 mt-8";
+    "rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20 mt-4";
 
   return (
     <>
@@ -48,21 +48,25 @@ export default function Home() {
               Explore the chicken nugget world!
             </h2>
           </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
-            <AuthShowcase />
+          <div className="rounded-md bg-gradient-to-t from-red/[0.3] to-light-nugget/[0.3] p-6 shadow-2xl drop-shadow-lg duration-200 hover:-translate-y-3 hover:skew-x-3">
+            <div className="flex flex-col items-center gap-2">
+              <p className="text-2xl text-white">
+                {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+              </p>
+              <AuthShowcase />
+            </div>
+          </div>
+          <div className="rounded-md bg-gradient-to-t from-red/[0.3] to-light-nugget/[0.3] p-6 px-12 shadow-2xl drop-shadow-lg duration-200 hover:translate-y-3 hover:skew-x-3">
+            <div className="flex flex-col items-center gap-2">
+              <p className=" text-2xl text-white">
+                Vote in the chicken nugget competition!
+              </p>
+              <Link href="/vote">
+                <button className={btn}>Vote</button>
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="">
-          <p className="tracking-tight text-white sm:text-[1.5rem]">
-            Vote in the chicken nugget competition!
-          </p>
-        </div>
-        <Link href="/vote">
-          <button className={btn}>Vote</button>
-        </Link>
         <RotatingImage />
         <div className="py-32"></div>
         <div className="">
@@ -91,8 +95,8 @@ export default function Home() {
         </div>
         <Image
           src="/images/kfc-logo.png"
-          width={981}
-          height={638}
+          width={981 * 0.5}
+          height={638 * 0.5}
           alt="red-x"
         ></Image>
         <div className="py-32"></div>
